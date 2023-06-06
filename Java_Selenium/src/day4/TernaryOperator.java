b521e477e8b0761ce9f4f02180c11e32060dfb9c
DharunRobots <125978419+DharunRobots@users.noreply.github.com>
2023-06-06 16:16:21 +0530

    Add files via upload

---
+++ Java_Selenium/src/day4/TernaryOperator.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+	
+		int marks=36;
+		
+		String result=marks>35 ? "Pass":"Fail";
+		
+		System.out.println(result);
+		
+
+	}
+
+}
